573e632a521ddb43d176816f6d3d516fb8b74117
philippninh@me.com <philippninh@me.com>
2016-02-09 20:52:56 +0100

    Add Youtube/Video section, but the scroll.graphic still don't work

---
+++ assets/js/functions.js
@@ -0,0 +1,12 @@
+$(window).scroll(function(){
+  vidScroll();
+  })
+
+
+  function vidScroll(){
+    var wScroll = $(window).scrollTop();
+
+    //console.log(wScroll);
+    $('.video-strip').css('background-position','center'+ wScroll +'px');
+  }
+  //install jquery in layouts, default.html <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
