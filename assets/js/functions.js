8ad9b2d34335bd1a5534e12a9da2e65a8bf7f5a9
philippninh@me.com <philippninh@me.com>
2016-02-12 11:54:31 +0100

    video section done

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,15 @@
 $(window).scroll(function(){
   vidScroll();
-  })
+});
 
 
   function vidScroll(){
     var wScroll = $(window).scrollTop();
 
     //console.log(wScroll);
-    $('.video-strip').css('background-position','center'+ wScroll +'px');
+    $('.video-strip').css('background-position','center -'+ wScroll +'px');
+
+        //console.log('$(.video-strip)', $('.video-strip').length);
+        //console.log('center '+ wScroll +'px');
   }
   //install jquery in layouts, default.html <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
